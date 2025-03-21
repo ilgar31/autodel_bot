93d155d6c1116c6e5892db9c98174110563c732c
ilgar31 <ilgar.bagishev@gmail.com>
2025-03-21 03:13:11 +0300

    last version

--- handlers/contact.py
+++ handlers/contact.py
@@ -1,12 +1,13 @@
 from aiogram import Router, types
 from aiogram import F  # Используем F для фильтрации
 from config import ADMINS
+from keyboards.main_menu import get_main_menu
+
 
 router = Router()
 
 @router.message(F.content_type == "contact")  # Фильтруем по типу контента
 async def receive_contact(message: types.Message):
-    await message.answer("✅ Спасибо! Администратор свяжется с вами.")
+    await message.answer("✅ Спасибо! Администратор свяжется с вами.", reply_markup=get_main_menu(message.from_user.id))
     for admin_id in ADMINS:
-        print(admin_id)
         await message.bot.send_message(admin_id, f"Пользователь оставил свои контактные данные для обратной связи.\n\n👤 Имя: {message.contact.first_name}\n☎️ Контакт: {message.contact.phone_number}\n📱 Telegram: @{message.from_user.username}")
